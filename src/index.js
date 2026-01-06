d458b6a6ffc5ff39873e1d1200f80a50743343a4
zolboo oktyabri <zolboo@zolboos-MacBook-Pro.local>
2026-01-06 10:26:07 +0800

    format

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import cors from 'cors'
-import { userRouter } from './routers/user-router.js';
-import { mongooseConnect } from './utils/mongoose-connect.js';
-import { foodRouter } from './routers/food-router.js';
-import { categoryRouter } from './routers/category-router.js';
+import express from "express";
+import cors from "cors";
+import { userRouter } from "./routers/user-router.js";
+import { mongooseConnect } from "./utils/mongoose-connect.js";
+import { foodRouter } from "./routers/food-router.js";
+import { categoryRouter } from "./routers/category-router.js";
 
 const app = express();
 const port = 3000;
@@ -12,10 +12,10 @@ mongooseConnect();
 app.use(cors());
 app.use(express.json());
 
-app.use('/users', userRouter);
-app.use('/food', foodRouter);
-app.use('/category', categoryRouter);
+app.use("/users", userRouter);
+app.use("/food", foodRouter);
+app.use("/category", categoryRouter);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
